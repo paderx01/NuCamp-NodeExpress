3a31afd673e7998e9dbb83fbb8b26f81c49b1e3f
Pader Xiong <paderx26@gmail.com>
2023-02-18 12:52:33 -0600

    workshop assignment - express router

--- routes/partnersRouter.js
+++ routes/partnersRouter.js
@@ -1,7 +1,7 @@
 const express = require("express");
-const campsiteRouter = express.Router();
+const partnersRouter = express.Router();
 
-campsiteRouter
+partnersRouter
   .route("/")
   .all((req, res, next) => {
     res.statusCode = 200;
@@ -9,42 +9,42 @@ campsiteRouter
     next();
   })
   .get((req, res) => {
-    res.end("Will send all the campsites to you");
+    res.end("Will send all the partners to you");
   })
   .post((req, res) => {
     res.end(
-      `Will add the campsite: ${req.body.name} with description: ${req.body.description}`
+      `Will add the partners: ${req.body.name} with description: ${req.body.description}`
     );
   })
   .put((req, res) => {
     res.statusCode = 403;
-    res.end("PUT operation not supported on /campsites");
+    res.end("PUT operation not supported on /partners");
   })
   .delete((req, res) => {
-    res.end("Deleting all campsites");
+    res.end("Deleting all partners");
   });
 
-campsiteRouter
-  .route("/campsiteId")
+partnerRouter
+  .route("/partnersId")
   .all((req, res, next) => {
     res.statusCode = 200;
     res.setHeader("Content-Type", "text/plain");
     next();
   })
   .get((req, res) => {
-    res.end("Will send all the campsites to you");
+    res.end("Will send all the partners to you");
   })
   .post((req, res) => {
     res.end(
-      `Will add the campsite: ${req.body.name} with description: ${req.body.description}`
+      `Will add the partners: ${req.body.name} with description: ${req.body.description}`
     );
   })
   .put((req, res) => {
     res.statusCode = 403;
-    res.end("PUT operation not supported on /campsites");
+    res.end("PUT operation not supported on /partners");
   })
   .delete((req, res) => {
-    res.end("Deleting all campsites");
+    res.end("Deleting all partners");
   });
 
-module.exports = campsiteRouter;
+module.exports = partnersRouter;
